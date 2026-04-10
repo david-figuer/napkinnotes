29253b5e7f954a197717310c8f2cffbf9bc3fc86
david-figuer <davidfiguer@usal.es>
2026-04-10 11:49:26 +0200

    Update pelicanconf.py

--- documentation/pelicanconf.py
+++ documentation/pelicanconf.py
@@ -65,8 +65,8 @@ TAGS_URL = "tags"
 CATEGORIES_URL = "categories"
 ARCHIVES_URL = "archives"
 SEARCH_URL = "search"
-NOSOTROS_URL="about"
-
+EXPLORAR_URL = "explorar"
+NOSOTROS_URL = "about"
 # Feeds
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
@@ -100,7 +100,7 @@ else:
     STATIC_PATHS.append("extra/robots_deny.txt")
     EXTRA_PATH_METADATA["extra/robots_deny.txt"] = {"path": "robots.txt"}
 
-DIRECT_TEMPLATES = ["index", "tags", "categories", "archives", "search", "404"]
+DIRECT_TEMPLATES = ["index", "tags", "categories", "archives", "search", "explorar", "404"]
 TAG_SAVE_AS = ""
 AUTHOR_SAVE_AS = ""
 CATEGORY_SAVE_AS = ""
